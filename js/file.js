702ad7cd6940861883160a5d7895edd3024e977e
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-03-20 12:16:15 +0100

    feat: added the HTML select

--- js/file.js
+++ js/file.js
@@ -36,13 +36,13 @@ function pariODispari() {
 function estrapolaNumRandom() {
     const minCeiled = Math.ceil(1);
     const maxFloored = Math.floor(5);
-    let numeroRandom = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+    let numeroRandom = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
     return numeroRandom;
 }
 
 
-function verificaCondizione(somma, secndoLutente) {
-    if (haVinto(somma, secndoLutente)) {
+function verificaCondizione(somma, secondoLutente) {
+    if (haVinto(somma, secondoLutente)) {
         alert("hai vinto")
     }
     else {
@@ -51,7 +51,33 @@ function verificaCondizione(somma, secndoLutente) {
     }
 }
 
-function haVinto(somma, secndoLutente) {
-    return somma % 2 == 0 && secndoLutente == "pari" || somma % 2 == 1 && secndoLutente == "dispari";
+function haVinto(somma, secondoLutente) {
+    return somma % 2 == 0 && secondoLutente == "pari" || somma % 2 == 1 && secondoLutente == "dispari";
 }
+
+
+
+
+/*creato la funzione che si occupa di caricare i documenti nel HTML */
+function creaHTMLOption() {
+    let id = document.getElementById("inputGroupSelect01");
+    for (let i = 0; i <= giochi.length; i++) {
+        let option = document.createElement("option");
+        id.appendChild(option);
+        option.innerHTML = giochi[i].nome;  
+        seleziona(id);
+
+    }
+
+}
+
+
+function seleziona(id) {
+ let select = id.selectedIndex;
+   let  selezionato = id.options[select];
+   
+}
+
+
+
 
