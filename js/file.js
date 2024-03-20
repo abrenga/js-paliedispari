0c1d777cca025e5fea45889e9dab9b24228b230d
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-03-20 17:37:54 +0100

    feat: added comments

--- js/file.js
+++ js/file.js
@@ -13,11 +13,11 @@ let giochi = [
 ];
 
 
-
+/*Sezione gioco svolto gli esercizi */
 function palindromo(parametro) {
     let reverse = parametro.split("").reverse().join("");
     if (reverse == parametro) {
-        creaHTMLEsercise( "è palindromo")
+        creaHTMLEsercise("è palindromo")
     } else {
         creaHTMLEsercise("non è palindromo")
     }
@@ -68,12 +68,13 @@ function creaHTMLOption(nome) {
 
 
 
-/*creato la funzione che si occupa di caricare i documenti nel HTML */
+/*Sezione per il funzionamento della pagina HTML.
+Creato la funzione che si occupa di caricare i documenti nel HTML */
 function estrapolaDati() {
     for (let i = 0; i <= giochi.length; i++) {
         let indice = giochi[i];
         creaHTMLOption(indice.nome);
-        giochi[i].esercizio 
+        giochi[i].esercizio
 
     }
 }
@@ -88,6 +89,7 @@ function seleziona() {
 }
 
 
+/*Ascoltiamo l'evento che ci selezionerà uno dei due giochi */
 id.addEventListener("change", (e) => {
     const inputUno = "cardEsercizioUno";
     const inputDue = "cardEsercizioDue";
@@ -102,40 +104,38 @@ id.addEventListener("change", (e) => {
 
 })
 
-
+/*Ascoltiamo l'evento che ci selezionerà uno dei due giochi */
 function show(nome, classE) {
     let row = document.getElementById(classE)
     let nomeSelezionato = seleziona();
     if (nome == nomeSelezionato) {
         row.classList.remove("d-none");
-    } else {
-        row.classList.add("d-none");
     }
 }
 
-
+/*al click del bottone dobbiamo rimuovere gli eventi ascoltati sul change */
 function remove(classE) {
     let row = document.getElementById(classE)
     row.classList.add("d-none");
 
 }
 
-
+/*Sezione Bottone per la visulizzazione dell'esercizio */
 let btnPalindromo = document.getElementById("btnPalindromo");
 
 btnPalindromo.addEventListener("click", (e) => {
     let inputNome = document.getElementById("inputNome").value;
     palindromo(inputNome)
-    ;
+        ;
 
 })
-
+/*creamo l'HTML necessaria a contenere il result primo esercizio */
 function creaHTMLEsercise(result) {
     const paragrafo = document.getElementById("paragrafo");
     paragrafo.innerHTML = result;
 }
 
-
+/*creamo l'HTML necessaria a contenere il result secvondo esercizio */
 function creaHTMLEserciseresult(result) {
     const paragrafo = document.getElementById("paragrafod");
     paragrafo.innerHTML = result;
@@ -143,13 +143,13 @@ function creaHTMLEserciseresult(result) {
 
 
 
-let btnseHaiVinto= document.getElementById("btnSeHaiVinto");
-
+let btnseHaiVinto = document.getElementById("btnSeHaiVinto");
+/*Sezione Bottone per la visulizzazione dell'esercizio */
 btnseHaiVinto.addEventListener("click", (e) => {
     let numeroInserito = document.getElementById("num").value;
     let pariDisapriStr = document.getElementById("result").value;
     pariODispari(numeroInserito, pariDisapriStr)
-    
+
 })
 
 
@@ -160,7 +160,8 @@ estrapolaDati()
 
 
 
-
+/*All'inizio avevo pensato di creare un array di oggetti e lavorare su quello, al momento penso che posso farlo anche senza, nei prossimi giorni
+ proverò a rendere il codice piè leggibile e se possibile piè snello */
 
 
 
